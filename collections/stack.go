44c646b7a5e7c11fd38bffa8dc01e3f7981613b7
rishav-ish <rishavkumar446@gmail.com>
2024-11-22 00:02:31 +0530

    make stack comparable genric; added recursive sort method in place

--- collections/stack.go
+++ collections/stack.go
@@ -35,33 +35,61 @@ func (s *Stack) Size() int {
 	return len(s.elements)
 }
 
-type StackV2 []any
+type StackV2[T comparable] []T
 
-func (s *StackV2) Push(val any) {
+func (s *StackV2[T]) Push(val T) {
 	*s = append(*s, val)
 }
 
-func (s *StackV2) Pop() (any, error) {
+func (s *StackV2[T]) Pop() (T, error) {
 	if len(*s) == 0 {
-		return 0, errors.New("stack is empty")
+		var zero_val T
+		return zero_val, errors.New("stack is empty")
 	}
 
 	top := (*s)[len(*s)-1]
-	*s = append([]any{}, (*s)[:len(*s)-1]...)
+	*s = append([]T{}, (*s)[:len(*s)-1]...)
 	return top, nil
 }
 
-func (s *StackV2) Top() (any, error) {
+func (s *StackV2[T]) Top() (T, error) {
 	if len(*s) == 0 {
-		return 0, errors.New("stack is empty")
+		var zero_val T
+		return zero_val, errors.New("stack is empty")
 	}
 	return (*s)[len(*s)-1], nil
 }
 
-func (s *StackV2) IsEmpty() bool {
+func (s *StackV2[T]) IsEmpty() bool {
 	return len(*s) == 0
 }
 
-func (s *StackV2) Size() int {
+func (s *StackV2[T]) Size() int {
 	return len(*s)
+}
+
+func (s *StackV2[T]) Sort(compare func(a, b T) bool) {
+	if s.Size() == 0 || s.Size() == 1 {
+		return
+	}
+
+	data, _ := s.Pop()
+	s.Sort(compare)
+	s.insert(data, compare)
+}
+
+func (s *StackV2[T]) insert(val T, compare func(a, b T) bool) {
+	if s.IsEmpty() {
+		s.Push(val)
+		return
+	}
+
+	if top_element, _ := s.Top(); compare(top_element, val) {
+		s.Push(val)
+		return
+	}
+
+	temp, _ := s.Pop()
+	s.insert(val, compare)
+	s.Push(temp)
 }
